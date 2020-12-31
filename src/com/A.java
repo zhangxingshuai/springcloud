fb2fa19b4cadd7f7ce7982dc729f7b3db619b02a
zhangxs <210968635@qq.com>
2020-12-31 12:55:59 +0800

    张兴帅1提交内容

---
+++ src/com/A.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * @author ZXS
+ * @version 1.0
+ * @description 类
+ * @date 2020-12-31 11:30
+ */
+public class A {
+
+    public void A1() {
+        System.out.println("这是zhangxingshuai创建的方法");
+    }
+}
